c1dea7360819c7d5f12286c685b7f34327711ae8
Jiwoon <jwninenine@gmail.com>
2025-02-20 14:44:25 +0900

    💄 style: main 태그 css 스타일 중첩되어 적용되는 거 수정

--- src/app/(user)/(service)/(auth)/layout.tsx
+++ src/app/(user)/(service)/(auth)/layout.tsx
@@ -2,11 +2,9 @@ import { ReactNode } from "react";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <main
-        className="mx-4 flex flex-grow flex-col md:mx-[30px]"
-        aria-labelledby="main-content"
+    <div
       >
       {children}
-    </main>
+    </div>
   );
 }
