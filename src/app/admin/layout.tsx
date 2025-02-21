7f8aecf727a2e466dd89b707b9f7648f3bbcd628
최민혁 <130155327+lactofreee@users.noreply.github.com>
2025-02-21 12:55:03 +0900

    💄 style: 코드 병합 및 shadcn 적용

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,10 +1,29 @@
 import { ReactNode } from "react";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/src/components/admin/ui/app-sidebar";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <div>
-      <h1>관리자 레이아웃</h1>
-      {children}
-    </div>
+    <SidebarProvider>
+      <AppSidebar />
+      <main>
+        <SidebarTrigger />
+        {children}
+      </main>
+    </SidebarProvider>
+    // <div className="flex h-[100vh] w-[100vw] bg-gray-300">
+    //   {/* 좌측 메뉴 영역 */}
+    //   <nav className="h-full w-[66px] bg-red"></nav>
+
+    //   {/* 우측 영역 */}
+    //   <div className="flex flex-grow flex-col">
+    //     {/* 상단 툴바 영역 */}
+    //     <div className="h-[73px] w-full bg-teal">dwa</div>
+    //     {/* 본문 영역 */}
+    //     <div className="mx-[20px] my-[22.5px] h-full rounded-[20px] bg-gray-100">
+    //       {children}
+    //     </div>
+    //   </div>
+    // </div>
   );
 }
