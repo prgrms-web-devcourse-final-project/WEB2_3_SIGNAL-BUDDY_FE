ac9fc2688b724c4b92be2803a5cfe6b4e3ba18ba
Jiwoon <jwninenine@gmail.com>
2025-02-21 12:40:50 +0900

    💄 style: my place 편집 화면 퍼블리싱

--- src/app/(user)/(service)/my-place/edit/page.tsx
+++ src/app/(user)/(service)/my-place/edit/page.tsx
@@ -1,3 +1,24 @@
-export default function Page(){
-    return <div className=""></div>;
+import Link from "next/link";
+import MyPlaceListEdit from "@/src/components/my-place/MyPlaceListEdit";
+
+export default function Page() {
+  return (
+    <div className="flex justify-center">
+      <div className="flex w-full max-w-[1240px] flex-col items-center justify-center">
+        <div className="border-grey-300 flex h-10 w-full items-center justify-between border-b text-sm font-extrabold mb-5">
+          <div className="flex">
+            <p className="text-grey-700">{`홈 > 나의 목적지`}</p>
+          </div>
+          <Link href={`/my-place`}>
+            완료
+          </Link>
+        </div>
+        <div className="  flex min-h-[917px] w-full">
+          <section className="flex flex-grow flex-col gap-2 ">
+            <MyPlaceListEdit />
+          </section>
+        </div>
+      </div>
+    </div>
+  );
 }
---
+++ src/components/my-place/MyPlaceListEdit.tsx
@@ -0,0 +1,78 @@
+import Image from "next/image";
+import deleteImage from "@/public/imgs/Delete.svg";
+import twoLineHamburger from "@/public/imgs/two-line-hamburger.svg";
+
+interface MyPlace {
+  id: string;
+  name: string;
+  address: string;
+}
+
+const myPlaceData: MyPlace[] = [
+  {
+    id: "1",
+    name: "집",
+    address: "경기 용인시 기흥구 기흥역로 63",
+  },
+  {
+    id: "2",
+    name: "회사",
+    address: "서울 강남구 테헤란로 427",
+  },
+];
+
+function MyPlaceItem({ name, address }: MyPlace) {
+  return (
+    <article>
+      {/* 모바일 화면 */}
+      <div className="flex items-center gap-2 md:hidden">
+        <div className="">
+          <Image src={deleteImage} alt="삭제" />
+        </div>
+        <div className="bg-white rounded-full pl-5 py-2 h-[58px] flex-1 min-w-[230px]">
+          <p className="text-base font-extrabold">{name}</p>
+          <p className="text-sm font-medium text-gray-500  ">{address}</p>
+        </div>
+        <div className="bg-white w-[58px] h-[58px] rounded-full flex items-center justify-center">
+          <div className="bg-gray-500 w-[50px] h-[50px] rounded-full "></div>
+        </div>
+        <div className="ml-1">
+          <Image src={twoLineHamburger} alt="순서 바꾸기 아이콘" />
+        </div>
+      </div>
+
+      {/* pc 화면 */}
+      <div className="hidden md:flex items-center gap-2 p-5 bg-white rounded-[20px] h-[80px] w-full justify-between">
+        <div className="flex">
+          <div className="flex items-center pr-4">
+            <Image src={deleteImage} alt="삭제" />
+          </div>
+          <div>
+            <p className="text-base font-extrabold">{name}</p>
+            <p className="text-sm font-medium text-gray-500  ">{address}</p>
+          </div>
+        </div>
+        
+        <div className="flex items-center">
+          <div className="bg-white w-[58px] h-[58px] rounded-xl border border-gray-300 flex items-center justify-center">
+            <div className="bg-gray-500 w-[50px] h-[50px] rounded-lg "></div>
+          </div>
+          <div className="ml-4">
+            <Image src={twoLineHamburger} alt="순서 바꾸기 아이콘" />
+          </div>
+        </div>
+      </div>
+
+    </article>
+  );
+}
+
+export default function MyPlaceListEdit() {
+  return (
+    <>
+      {myPlaceData.map((myPlace, index) => (
+        <MyPlaceItem key={index} {...myPlace} />
+      ))}
+    </>
+  );
+}
