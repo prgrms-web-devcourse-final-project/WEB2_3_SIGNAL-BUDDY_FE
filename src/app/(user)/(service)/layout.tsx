04801f8e5551db4f56ee46b68d0187b6575adfb6
junyoung <jadw9715@gmail.com>
2025-02-27 12:26:16 +0900

    ✨Feat. laytout 수정

--- src/app/(user)/(service)/layout.tsx
+++ src/app/(user)/(service)/layout.tsx
@@ -10,12 +10,12 @@ export default function UserRootLayout({ children }: Props) {
       <header
         role="banner"
         aria-label="주요 네비게이션"
-        className="w-[calc(100vw-32px)] md:w-[calc(100vw-60px)] flex h-[70px] max-w-[1240px] justify-center mx-auto sticky top-0 left-0 right-0"
+        className="w-[calc(100vw-32px)] md:w-[calc(100vw-60px)] flex h-[70px] max-w-[1240px] justify-center mx-auto sticky top-0 left-0 right-0 z-[9999]"
       >
         <Navbar />
       </header>
       <main
-        className="mx-4 flex flex-grow flex-col md:mx-[30px]"
+        className="w-[calc(100vw-32px)] md:w-[calc(100vw-60px)] flex flex-grow flex-col mx-auto"
         aria-labelledby="main-content"
       >
         {children}
