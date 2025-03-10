5132962e6558cd0dcfe76fc326d26d4745a8726b
최민혁 <130155327+lactofreee@users.noreply.github.com>
2025-03-10 11:16:29 +0900

    ✨ feat: 피드백 리스트 페이지네이션 기능 추가

---
+++ src/components/feedback/ui/FeedbackPagination.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination";
+
+export default function FeedbackPagination({
+  totalPages,
+  page,
+  size,
+}: {
+  totalPages: number;
+  page: number;
+  size: number;
+}) {
+  return (
+    <Pagination>
+      <PaginationContent>
+        {/* 이전 페이지 버튼 */}
+        <PaginationItem>
+          <PaginationPrevious href={`?page=${Math.max(page - 1, 1)}`} />
+        </PaginationItem>
+
+        {/* 페이지 번호 버튼 */}
+        {[...Array(totalPages)].map((_, idx) => {
+          const pageNumber = idx + 1;
+          return (
+            <PaginationItem key={idx}>
+              <PaginationLink href={`?page=${pageNumber - 1}`}>
+                {pageNumber}
+              </PaginationLink>
+            </PaginationItem>
+          );
+        })}
+
+        {totalPages > 5 && (
+          <PaginationItem>
+            <PaginationEllipsis />
+          </PaginationItem>
+        )}
+
+        {/* 다음 페이지 버튼 */}
+        <PaginationItem>
+          <PaginationNext href={`?page=${Math.min(page + 1, totalPages)}`} />
+        </PaginationItem>
+      </PaginationContent>
+    </Pagination>
+  );
+}
